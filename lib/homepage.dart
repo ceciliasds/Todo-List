b9a57df6d712802af4c778cd7509bcea0ac4fd3f
Mallari-Aizareel <aizareelmallari000@gmail.com>
2024-04-19 12:24:35 +0800

    Mallari, Aizareel

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'addCategory.dart';
+import 'CategoryList.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'To-Do List',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white70),
+        ),
+        backgroundColor: Color.fromARGB(255, 75, 55, 42),
+        centerTitle: true,
+        leading: Container(),
+      ),
+      backgroundColor: Color.fromRGBO(243, 230, 202, 1.0),
+      body: CategoryList(),
+      floatingActionButton: AddCategoryButton(),
+    );
+  }   
+}
 
